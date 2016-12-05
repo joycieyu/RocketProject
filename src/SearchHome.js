3a96b2f92b44b3e640153a97e3fe761937a00aac
Joycie Yu <joycie@uw.edu>
2016-12-04 23:36:24 -0800

    switch back to search bar

--- src/SearchHome.js
+++ src/SearchHome.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
+import { AutoComplete }   from 'material-ui';
 import './SearchHomeStyle.css';
 
 
@@ -12,27 +13,24 @@ const styles = {
 
 class SearchHome extends Component {
     
- state = {
-    value: 1,
-  };
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dataSource: [],
+    };
+  }
 
   handleChange = (event, index, value) => this.setState({value});
     render() {
         return (
-            <div className="centered container">
+        <div className="centered container">
             <p>It's Lit Fam</p>
-            <SelectField 
-            floatingLabelText="Select Mood"
-            value={this.state.value}
-            onChange={this.handleChange}
-            autoWidth={true}
-            >
-                <MenuItem value={1} primaryText="Happy" />
-                <MenuItem value={2} primaryText="Angry" />
-                <MenuItem value={3} primaryText="Calm" />
-                <MenuItem value={4} primaryText="Sad" />
-                <MenuItem value={5} primaryText="Surprised" />
-            </SelectField>
+             <AutoComplete hintText="Type your mood here..." 
+                     dataSource={this.state.dataSource} 
+                     onUpdateInput={this.onUpdateInput} 
+                     onNewRequest={this.onNewRequest}
+                  />
         </div>
         );
     }
